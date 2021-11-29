70a2f56523d84c27504503173b77b0407a24c8d6
Jolijn <jolijnrosendaal@gmail.com>
2021-11-29 16:50:59 +0100

    codecademy opdracht 8

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ContactPicker } from '../contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
@@ -19,5 +20,31 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
   };
 
-  return <h3>This is a appointment form</h3>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="title"
+          value={title}
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          placeholder="date"
+          value={date}
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          min={getTodayString()}
+        />
+        <input
+          placeholder="time"
+          value={time}
+          type="time"
+          onChange={(e) => setTime(e.target.value)}
+        />
+        <ContactPicker contacts={contacts} />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 };
